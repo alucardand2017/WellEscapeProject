39d2d7d50bef97afbe51df9775009e480f427ff4
Anderson do Nascimento <anderson_ns5@hotmail.com>
2020-04-09 13:44:41 -0300

    Criado Animations e Script do Player
    Realizado o Slice dos Sprites do Player, Criado as principais animações do Player
    Iniciado mecanicas de movimentação

---
+++ WellEscape/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rb2D; //criação de variável de manipulação
+    float horizontal;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal"); 
+        Movimentar(horizontal); //chama a função de movimentação do Player
+    }
+
+
+
+
+   private void Movimentar(float h) // função movimentar do player
+    {
+        rb2D.velocity = new Vector2(h, rb2D.velocity.y);
+    }
+
+
+
+
+
+
+}
