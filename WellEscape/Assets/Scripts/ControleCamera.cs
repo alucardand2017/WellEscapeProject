d0c36f0cdbf1e457ec6c2bfa22871c61d77f53ae
VandersonB <vanderson3e@gmail.com>
2020-04-13 00:06:48 -0300

    Controle de Câmera e renomeacao de variaveis

---
+++ WellEscape/Assets/Scripts/ControleCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleCamera : MonoBehaviour
+{
+    //Objetivo: criar uma camera que acompanhe o jogador, com ele centralizado. Ele precisa, além disso, perceber quando atinge o final da tela e parar de acompanhar o jogador.
+    // Prosposta: verificar se a câmera atingiu o limite de borda, definido por um GO na cena (LimiteDaCamera) e, quando isso ocorrer, a câmera deixa de acompanhar o jogador.
+    [SerializeField]
+    private Transform jogador;
+    [SerializeField]
+    private GameObject limiteCamera;
+
+    private float alturaDaCamera;
+    private float larguraDaCamera;
+
+    void Awake()
+    {
+        alturaDaCamera = Camera.main.orthographicSize;
+        larguraDaCamera = alturaDaCamera * Camera.main.aspect;
+        Camera.main.transform.position = new Vector3(jogador.position.x, jogador.position.y, Camera.main.transform.position.z);
+    }
+    void Update()
+    {
+        //Se a camera não estiver na borda
+        if (jogador.position.x + larguraDaCamera < limiteCamera.transform.position.x + limiteCamera.GetComponent<BoxCollider2D>().size.x / 2)
+        {
+            if (jogador.position.x - larguraDaCamera > limiteCamera.transform.position.x - limiteCamera.GetComponent<BoxCollider2D>().size.x / 2)
+            {
+                SeguirJogadorHorizontal();
+            }
+        }
+            if (jogador.position.y + alturaDaCamera > limiteCamera.transform.position.y + limiteCamera.GetComponent<BoxCollider2D>().size.y / 2)
+            {
+                SeguirJogadorVertical();
+            }        
+    }
+
+    private void SeguirJogadorHorizontal()
+    {
+        Camera.main.transform.position = new Vector3(jogador.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+    }
+    private void SeguirJogadorVertical()
+    {
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, jogador.position.y, Camera.main.transform.position.z);
+    }
+  
+}
+
--- WellEscape/Assets/Scripts/Player.cs
+++ WellEscape/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Vanderson(12/04) - alterado ordenamento das variáveis (Serializadas, privadas, publicas) e diferenciado o seu nome, no caso, colocado as públicas em CapsLock para saber a diferenciação.
 public class Player : MonoBehaviour
 {
     [SerializeField] // para aparecer no inspector do player
@@ -12,18 +13,14 @@ public class Player : MonoBehaviour
     private bool grounded; //variavel de controle do pulo (condição para pular)
     
     private bool jumping;   //acessa o pulo do jogador
-    
-    
-    public Transform groundCheck; //definida pela posição do Gameobjetic "GroundCheck" do Player,
-    public Transform axeAttack;   //definida pela posição do Gameobjectic "AxeAttack" do Player
-    public float raioPulo;        //define o raio de ação do CheckGound do Player para o pulo
-    
-    
-    
     private Rigidbody2D rb2D; //criação de variável de manipulação do rigidbody do player
     private bool eLadoDireito; //parametro para identificar o lado q o player está virado para o pivotamento de sprite
     private Animator animator; //criação de variavel de manipulaçao do animator
-    float horizontal;           //variavel para controlar player 1 Eixo X. 
+    private float horizontal;  //variavel para controlar player 1 Eixo X.
+
+    public Transform GroundCheck; //definida pela posição do GameObject "GroundCheck" do Player,
+    public Transform AxeAttack;   //definida pela posição do GameObject "AxeAttack" do Player
+    public float RaioPulo;        //define o raio de ação do CheckGound do Player para o pulo
     
     private void Awake() {
         rb2D = GetComponent<Rigidbody2D>();         //Coleta os componentes do player
@@ -34,8 +31,9 @@ public class Player : MonoBehaviour
     }
     void Update(){
         //grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-        grounded = Physics2D.OverlapCircle(groundCheck.position, raioPulo, 1 << LayerMask.NameToLayer("Ground"));
-        if(Input.GetKeyDown(KeyCode.W) && grounded) {
+        grounded = Physics2D.OverlapCircle(GroundCheck.position, RaioPulo, 1 << LayerMask.NameToLayer("Ground"));
+        if(Input.GetKeyDown(KeyCode.W) && grounded) 
+        {
             jumping = true;
         }
     }
@@ -44,26 +42,30 @@ public class Player : MonoBehaviour
         Movimentar(horizontal);                     //funçao de deslocamento que recebe o valor do eixo X (-1~1)
         MudarDirecao(horizontal);                   //função de direção que recebe o valor do eixo X (-1~1)
         Pulo(jumping);
-        Aminacao(rb2D.velocity.x);
+        Animacao(rb2D.velocity.x);
     }
     //******************************FUNÇÕES PARA OS CONTROLES***************************************************
     private void Pulo(bool j){                                      //PULO DO PLAYER
-        if(j){                                
+        if(j)
+        {                                
             rb2D.AddForce(transform.up * forcapulo, ForceMode2D.Impulse);   
             
             jumping = false;
         }
     }
-    private void Aminacao(float h){                                 //CONTROLE DAS ANIMAÇÕES DO PLAYER
+    private void Animacao(float h) //CONTROLE DAS ANIMAÇÕES DO PLAYER
+    {                                
         if(!grounded) animator.SetBool("pulando", true);            //Se não estiver no chão anima o pulo
         if(grounded) animator.SetBool("pulando", false);            // --- situação oposta ...
         animator.SetFloat("velocidade",Mathf.Abs(h));               //Se tiver velocidade abs > 0 anima corrida
     }
-    private void Movimentar(float h){                               //MOVIMENTAÇÃO LATERAL DO PLAYER
+    private void Movimentar(float h)//MOVIMENTAÇÃO LATERAL DO PLAYER
+    {                               
         rb2D.velocity = new Vector2(h*velocidade, rb2D.velocity.y); //parametro velocidade do rb2D = (eixo X * velocidade, mantem eixo y)              
     }
 
-    private void MudarDirecao (float horizontal) {                              //VIRADA DE SPRITE DO PLAYER
+    private void MudarDirecao (float horizontal) //VIRADA DE SPRITE DO PLAYER
+    {                              
         if(horizontal > 0 && !eLadoDireito || horizontal < 0 && eLadoDireito) { // se (X > 0 && sprite pra esquerda < 0 OU X < 0 && sprite pra direita)
             eLadoDireito = !eLadoDireito;                                       // inverte o argumento e o sprite
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);// --
@@ -77,6 +79,6 @@ public class Player : MonoBehaviour
         Debug.Log("PAROU DE COLIDIR com " + collision2D.gameObject.tag);    
     }
     void OnDrawGizmos() {                                               //desenha a esfera de detecção do chão para o pulo, apenas para visualização
-        Gizmos.DrawWireSphere(groundCheck.position, raioPulo);  
+        Gizmos.DrawWireSphere(GroundCheck.position, RaioPulo);  
     }
 }
